47d9bd01320d95df3246e56ade04e400ac6ee07a
SergioEvando23 <sergioevandocosta@gmail.com>
2021-12-19 14:13:31 -0300

    requisito 3 implementar a função VQV

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,11 +25,12 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
+    fail('Test incompleto')
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
+    // expect(circle('string').toBeUndefined(true));
     // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado tem 3 propriedades.
+    // expect(typeOf circle(7)).toBe('object');
+      // Teste se o objeto retornado tem 3 propriedades.
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
