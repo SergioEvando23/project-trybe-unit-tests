c3eadb2f09b41caddf10b5d307f13a00e9b7b04c
SergioEvando23 <sergioevandocosta@gmail.com>
2021-12-20 19:21:39 -0300

    Requisito 9: implementado o test getChacacter

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -43,13 +43,40 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
-    // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
-    // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
-    // Teste se os parâmetros não são Case Sensitive.
-    // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    expect(getCharacter()).toBeUndefined();
   });
+  it('Teste se a função retorna o objeto correto para o parâmetro "Arya"',() => {
+    // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    expect(getCharacter('Arya')).toEqual({
+      name: 'Arya Stark',
+      class: 'Rogue',
+      phrases: ['Not today', 'A girl has no name.'] 
+    })
+  });
+  it('Teste se a função retorna o objeto correto para o parâmetro "Brienne"', () => {
+    // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    expect(getCharacter('Brienne')).toEqual({
+      name: 'Brienne Tarth',
+      class: 'Knight',
+      phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'] 
+    })
+  });
+  it('Teste se a função retorna o objeto correto para o parâmetro "Melissandre"', () => {
+    // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    expect(getCharacter('Melissandre')).toEqual({
+      name: 'Melissandre',
+      class: 'Necromancer',
+      phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'] 
+    })
+  });
+  it('Teste se os parâmetros não são Case Sensitive.', () => {
+    // Teste se os parâmetros não são Case Sensitive.
+    expect(getCharacter('Arya')).toEqual(getCharacter('aRYa'))
+  });
+  it('Teste se ao passar um nome que não está na tabela, a função retorna undefined', () => {
+    // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    expect(getCharacter('Sergio')).toBeUndefined();
+  })
 });
